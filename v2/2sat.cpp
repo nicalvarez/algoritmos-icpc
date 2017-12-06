4510ced53ae7f9bc9f388a3d6952c2a4ac950428
Nicolas Alvarez <nico.alvarez@gmail.com>
2017-12-06 09:40:23 -0300

    2-SAT

---
+++ v2/2sat.cpp
@@ -0,0 +1,50 @@
+int n;
+vector < vector<int> > g, gt;
+vector<bool> used;
+vector<int> order, comp;
+
+void dfs1 (int v) {
+	used[v] = true;
+	for (size_t i=0; i<g[v].size(); ++i) {
+		int to = g[v][i];
+		if (!used[to])
+			dfs1 (to);
+	}
+	order.push_back (v);
+}
+
+void dfs2 (int v, int cl) {
+	comp[v] = cl;
+	for (size_t i=0; i<gt[v].size(); ++i) {
+		int to = gt[v][i];
+		if (comp[to] == -1)
+			dfs2 (to, cl);
+	}
+}
+
+int main() {
+	... чтение n, графа g, построение графа gt ...
+
+	used.assign (n, false);
+	for (int i=0; i<n; ++i)
+		if (!used[i])
+			dfs1 (i);
+
+	comp.assign (n, -1);
+	for (int i=0, j=0; i<n; ++i) {
+		int v = order[n-i-1];
+		if (comp[v] == -1)
+			dfs2 (v, j++);
+	}
+
+	for (int i=0; i<n; ++i)
+		if (comp[i] == comp[i^1]) {
+			puts ("NO SOLUTION");
+			return 0;
+		}
+	for (int i=0; i<n; ++i) {
+		int ans = comp[i] > comp[i^1] ? i : i^1;
+		printf ("%d ", ans);
+	}
+
+}
