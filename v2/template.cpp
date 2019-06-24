9ed14c62fe438ddb80b6d978b80e178214104fcc
Nicolas Alvarez <nicolas@Nicolass-MacBook-Air.local>
2019-06-24 16:52:54 -0300

    template.cpp

---
+++ v2/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,n) for(int i=0;i<(int)(n);i++)
+#define si(c) ((int)(c).size())
+#define forsn(i,s,n) for(int i = (int)(s); i<((int)n); i++)
+#define dforsn(i,s,n) for(int i = (int)(n)-1; i>=((int)s); i--)
+#define all(c) (c).begin(), (c).end()
+#define D(a) cerr << #a << "=" << a << endl;
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    return 0;
+}
