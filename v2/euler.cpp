69ca41357d607e95995eb42338e420e8151f7952
Nicolas Alvarez <nico.alvarez@gmail.com>
2020-11-29 19:02:13 -0300

    Improvements to euler tour/path
    * Included support for paths
    * Bug fixes:
        - start dfs from a non-isolated vertex
        - remove const qualifier from adj parameter in dfs

--- v2/euler.cpp
+++ v2/euler.cpp
@@ -18,7 +18,7 @@ typedef pair<int,int> pii;
 namespace euler {
 
     vi tour, dead;
-    void dfs(const vector<pii> &e, const vector<vi> &adj, int u) {
+    void dfs(const vector<pii> &e, vector<vi> &adj, int u) {
         while (!adj[u].empty()) {
             auto idx = adj[u].back();
             adj[u].pop_back();
@@ -30,41 +30,97 @@ namespace euler {
         }
     }
 
-    vi undirected(int n, const vector<pii> &e) {
+    // Looks for an eulerian tour (not path)
+    // returns a vector of indexes of edges
+    vi undirected_tour(int n, const vector<pii> &e) {
         vector<vi> adj(n);
         vi deg(n);
+        int start = 0;
         forn(i,si(e)) {
             int u = e[i].first, v = e[i].second;
             adj[u].pb(i);
             adj[v].pb(i);
             deg[u]++; deg[v]++;
+            start = u;
         }
         forn(u,n) if (deg[u]%2) return vi();
         
         tour.clear();
         dead = vi(si(e));
-        dfs(e,adj,0);
+        dfs(e,adj,start);
         if (si(tour) != si(e)) return vi();
         reverse(all(tour));
         return tour;
     }
 
-    vi directed(int n, const vector<pii> &e) {
+    // Looks for an eulerian path
+    // returns a vector of indexes of edges
+    vi undirected_path(int n, vector<pii> e) {
+        vi deg(n);
+        forn(i,si(e)) {
+            int u = e[i].first, v = e[i].second;
+            deg[u]++; deg[v]++;
+        }
+        vi odd;
+        forn(u,n) if (deg[u]%2) odd.pb(u);
+        if (si(odd) > 2) return vi();
+        if (odd.empty()) return undirected_tour(n, e);
+
+        // path
+        assert(si(odd) == 2);
+        int sz = si(e);
+        e.eb(odd[0], odd[1]);
+        auto ans = undirected_tour(n, e);
+        if (si(ans) < si(e)) return vi();
+        ans.erase(find(all(ans), sz));
+        return ans;
+    }
+
+    // Looks for an eulerian tour (not path)
+    // returns a vector of indexes of edges
+    vi directed_tour(int n, const vector<pii> &e) {
         vector<vi> adj(n);
         vi bal(n);
+        int start = 0;
         forn(i,si(e)) {
             int u = e[i].first, v = e[i].second;
             adj[u].pb(i);
             bal[u]--; bal[v]++;
+            start = u;
         }
         forn(u,n) if (bal[u] != 0) return vi();
 
         tour.clear();
         dead = vi(si(e));
-        dfs(e,adj,0);
+        dfs(e,adj,start);
         if (si(tour) != si(e)) return vi();
         reverse(all(tour));
         return tour;
+    }
+
+    // Looks for an eulerian path
+    // returns a vector of indexes of edges
+    vi directed_path(int n, vector<pii> e) {
+        vi bal(n);
+        forn(i,si(e)) {
+            int u = e[i].first, v = e[i].second;
+            bal[u]--; bal[v]++;
+        }
+        vi neg, pos;
+        forn(u,n) {
+            if (bal[u] < 0) neg.pb(u);
+            if (bal[u] > 0) pos.pb(u);
+        }
+        if (neg.empty() && pos.empty()) return directed_tour(n, e);
+        if (si(neg) > 1 || si(pos) > 1) return vi(); 
+        if (bal[neg[0]] < -1 || bal[pos[0]] > 1) return vi();
+
+        int sz = si(e); 
+        e.eb(pos[0], neg[0]);
+        auto ans = directed_tour(n, e);
+        if (si(ans) < si(e)) return vi();
+        ans.erase(find(all(ans), sz));
+        return ans;
     }
 }
 
